bb03c740a39edf6066e1a47e344c163858ca73c6
abdus shobhan <shobhanrahman21@gmail.com>
2024-10-18 00:25:08 +0600

    first commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+// let userName ="abdus_shobhan";
+// let age =34;
+// let arrat = [5,6,7]
+
+// console.log(userName);
+// console.log(age);
+// console.log(arrat);
+
+// let userId:number
+// let firstName:string
+// let lastName:string
+// let isActivate :true
+
+// userId = 12;
+// firstName= "Abdus";
+// lastName=" Shobhan";
+// isActivate= true
+
+// console.log(`Youre User id ${userId} and your name ${firstName} ${lastName} and status ${isActivate}`);
+
+// function display(){
+//     console.log("Hi i am void");
+// }
+// display();
+
+// Here is emplement User Defiend Data Type in typeScript 
+
+// let Userid :(number|string|boolean);
+
+// Userid:"Shobhan";
+// Userid:101;
+// isActive:true;
+
+// function displayuser(Userid){
+//     console.log(Userid)
+
+
+// }
+
+// displayuser("shobhan")
+// displayuser(101)
+
+// array decleration in TS
+
+let UserNames:string[];
+
+UserNames = ["anis","rabu","Pinky"];
+
+console.log(UserNames);
+
