fe3f0558c0004ca84b60c55655e0c64ea417f1f1
abdus shobhan <shobhanrahman21@gmail.com>
2024-10-21 00:23:01 +0600

    typescript class

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // let userName ="abdus_shobhan";
 // let age =34;
 // let arrat = [5,6,7]
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // console.log(userName);
 // console.log(age);
 // console.log(arrat);
@@ -28,6 +43,62 @@
 // displayuser("shobhan")
 // displayuser(101)
 // array decleration in TS
-var UserNames;
-UserNames = ["anis", "rabu", "Pinky"];
-console.log(UserNames);
+// let UserNames:string[];
+// UserNames = ["anis","rabu","Pinky","Shobhan","fuck"];
+// console.log(UserNames);
+// Here is emplement the object type data Basic
+var user;
+user = [];
+var user1;
+user1 = { userName: "Shonhan", userId: 101 };
+user.push(user1);
+var user2;
+user2 = { userName: "Abdus_Shobhan", userId: 103 };
+user.push(user2);
+console.log(user);
+var getRequest;
+getRequest = "GET";
+console.log(getRequest);
+// This is constroctor of this class...
+var User = /** @class */ (function () {
+    function User(userName, age) {
+        this.userName = userName;
+        this.age = age;
+    }
+    User.prototype.display = function () {
+        console.log("userName: ".concat(this.userName, " and age ").concat(this.age));
+    };
+    return User;
+}());
+// let user3= new User("shobhan",32);
+// let user4 = new User("md sakib",32);
+// user3.display();
+// user4.display();
+// This is for class method and object in TS
+// this is a inherate class of ts 
+var pcUser = /** @class */ (function () {
+    function pcUser(userName, age) {
+        this.userName = userName;
+        this.age = age;
+    }
+    pcUser.prototype.display1 = function () {
+        console.log("userName: ".concat(this.userName, " age: ").concat(this.age));
+    };
+    return pcUser;
+}());
+// const u = new pcUser("abdus_shobhanCSEDUET",4);
+// u.display1();
+var studet = /** @class */ (function (_super) {
+    __extends(studet, _super);
+    function studet(userName, age, studetnID) {
+        var _this = _super.call(this, userName, age) || this;
+        _this.studetnID = studetnID;
+        return _this;
+    }
+    studet.prototype.display2 = function () {
+        console.log("userName: ".concat(this.userName, " age: ").concat(this.age, " and id ").concat(this.studetnID));
+    };
+    return studet;
+}(pcUser));
+var student = new studet("abdus_shobhan", 404, 10123);
+student.display2();
--- index.ts
+++ index.ts
@@ -42,9 +42,104 @@
 
 // array decleration in TS
 
-let UserNames:string[];
+// let UserNames:string[];
 
-UserNames = ["anis","rabu","Pinky"];
+// UserNames = ["anis","rabu","Pinky","Shobhan","fuck"];
 
-console.log(UserNames);
+// console.log(UserNames);
 
+
+// Here is emplement the object type data Basic
+
+
+let user : object[];
+user =[];
+let user1:{userName:string; userId:number};
+
+user1 = {userName:"Shonhan",userId:101};
+
+user.push(user1);
+let user2 :{userName:string;userId:number};
+
+user2 = {userName:"Abdus_Shobhan",userId:103}
+user.push(user2);
+
+console.log(user);
+
+
+type RequestType = "GET"|"POST"
+let getRequest:RequestType;
+
+
+getRequest="GET";
+console.log(getRequest);
+// This is constroctor of this class...
+class User{
+    userName:string;
+    age:number;
+
+
+    constructor(userName,age){
+        this.userName = userName;
+        this.age= age;
+    }
+
+     display(){
+
+       console.log(`userName: ${this.userName} and age ${this.age}`)
+    }
+}
+
+// let user3= new User("shobhan",32);
+// let user4 = new User("md sakib",32);
+
+// user3.display();
+// user4.display();
+// This is for class method and object in TS
+
+// this is a inherate class of ts 
+
+class pcUser{
+    userName:string;
+    age:number
+
+    constructor(userName,age){
+
+        this.userName = userName;
+        this.age = age;
+
+    }
+
+    display1(){
+        console.log(`userName: ${this.userName} age: ${this.age}`)
+    }
+}
+
+// const u = new pcUser("abdus_shobhanCSEDUET",4);
+// u.display1();
+
+class studet extends pcUser {
+    studetnID:number;
+
+    constructor(userName,age,studetnID){
+
+        super(userName,age);
+
+        this.studetnID = studetnID;
+
+
+      
+
+    }
+
+    display2(){
+        console.log(`userName: ${this.userName} age: ${this.age} and id ${this.studetnID}`)
+    }
+
+    
+
+}
+
+let student = new studet("abdus_shobhan",404,10123);
+
+student.display2();
